aa0f7b1bf7c3ff87d911e8838648fa5ff46d596b
Alan Norton <alan@mealpal.com>
2018-10-14 15:07:32 -0700

    rails webpacker:install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,3 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
-
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const application = Application.start()
-const context = require.context("controllers", true, /.js$/)
-application.load(definitionsFromContext(context))
